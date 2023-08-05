e2139371ec3841038e50b8b6001c330033483334
Hrithik <slumdog4321@gmail.com>
2023-08-05 17:46:12 +0530

    Value COmponent Completed

--- src/components/ValueDiv.jsx
+++ src/components/ValueDiv.jsx
@@ -1,7 +1,66 @@
 import React from "react";
 
 const ValueDiv = () => {
-  return <div>ValueDiv</div>;
+  return (
+    <div className="mb-[4rem] mt-[6rem]">
+      <h1 className="text-textColor text-[25px] py-[2rem] pb-[3rem] w-[400px] block font-bold">
+        The Value that holds us true and to account
+      </h1>
+      <div className="grid gap-[5rem] grid-cols-3 items-center">
+        <div className="singleGrid rounded-[10px] hover:bg-[#eeedf7] p-[1.5rem]">
+          <div className="flex items-center gap-3">
+            <div
+              className="imgDiv p-4 h-[60px] w-[60px] flex justify-center items-center
+            rounded-[.8rem] bg-inherit-[#dedef8]"
+            >
+              <img
+                src="https://static.vecteezy.com/system/resources/previews/012/871/379/original/dell-brand-icon-illustration-top-tech-company-logotype-free-png.png"
+                alt="logo"
+              />
+            </div>
+            <span className="font-semibold text-textColor text-[18px]">Simplicity</span>
+          </div>
+          <p className="text-textColor text-[13px] opacity-[.7] py-[1rem] font-semibold">
+            Things Are Naturaly beatuful, we Just need That Prespective
+          </p>
+        </div>
+        <div className="singleGrid rounded-[10px] hover:bg-[#eeedf7] p-[1.5rem]">
+          <div className="flex items-center gap-3">
+            <div
+              className="imgDiv p-4 h-[60px] w-[60px] flex justify-center items-center
+            rounded-[.8rem] bg-inherit-[#dedef8]"
+            >
+              <img
+                src="https://static.vecteezy.com/system/resources/previews/012/871/379/original/dell-brand-icon-illustration-top-tech-company-logotype-free-png.png"
+                alt="logo"
+              />
+            </div>
+            <span className="font-semibold text-textColor text-[18px]">Simplicity</span>
+          </div>
+          <p className="text-textColor text-[13px] opacity-[.7] py-[1rem] font-semibold">
+            Things Are Naturaly beatuful, we Just need That Prespective
+          </p>
+        </div>
+        <div className="singleGrid rounded-[10px] hover:bg-[#eeedf7] p-[1.5rem]">
+          <div className="flex items-center gap-3">
+            <div
+              className="imgDiv p-4 h-[60px] w-[60px] flex justify-center items-center
+            rounded-[.8rem] bg-inherit-[#dedef8]"
+            >
+              <img
+                src="https://static.vecteezy.com/system/resources/previews/012/871/379/original/dell-brand-icon-illustration-top-tech-company-logotype-free-png.png"
+                alt="logo"
+              />
+            </div>
+            <span className="font-semibold text-textColor text-[18px]">Simplicity</span>
+          </div>
+          <p className="text-textColor text-[13px] opacity-[.7] py-[1rem] font-semibold">
+            Things Are Naturaly beatuful, we Just need That Prespective
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ValueDiv;
